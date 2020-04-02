dee235ee5b6e9ca1366635acee561b416008ccf8
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-02 11:09:15 -0400

    adding exercise 1 of python103

---
+++ python103.s.1.py
@@ -0,0 +1,11 @@
+# python 103, small exercise #1
+
+#use a while loop to print numbers 1 thru 10
+
+# defining counter
+count = 0
+
+# looping through numbers
+while count < 11:
+    print(count)
+    count += 1
