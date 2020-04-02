731eb136c0f1681c2f1274f20bdef599b7e876f1
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-04-02 12:50:21 -0400

    adding 3rd exercise of Python103-small

---
+++ python103.s.3.py
@@ -0,0 +1,15 @@
+#Python103, small, exercise 3
+
+#Print a 5x5 square of * characters
+
+# defining var to hold '*' string
+aster = '*****'
+
+# defining counter
+count = 0
+
+# printing aster based on # of iterations specified
+
+while count < 5:
+    print(aster)
+    count += 1
